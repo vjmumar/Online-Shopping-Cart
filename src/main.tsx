b575a6da02f2a258317f6bd9f37689a48f4a56c2
Virgilio N. Mumar JR <pinoyvj39@gmail.com>
2023-11-16 00:25:34 +0800

    redux-persistor implemented

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,19 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 // Store
 import { store } from "@store/index.ts";
+import { persistStore } from "@store/index.ts";
 // Redux
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
+			<PersistGate persistor={persistStore}>
+				<BrowserRouter>
+					<App />
+				</BrowserRouter>
+			</PersistGate>
 		</Provider>
 	</React.StrictMode>,
 );
--- src/store/index.ts
+++ src/store/index.ts
@@ -4,12 +4,30 @@ import {
 	TypedUseSelectorHook,
 	useSelector as reduxSelector,
 } from "react-redux";
+// Redux persist
+import {
+	persistStore as persistStoreFunction,
+	persistReducer,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 // Types
 import { RootState } from "./types";
 
+const persistConfig = {
+	key: "store",
+	storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Our store
 export const store = configureStore({
-	reducer: rootReducer,
+	reducer: persistedReducer,
 });
+
+// OUr persist store
+export const persistStore = persistStoreFunction(store);
+
 // To access the store
 export const getStoreState = store.getState() as RootState;
 // To access store inside a component
